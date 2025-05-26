991eb8f99f0cac6095da8160b643179182fa7171
bog739 <bogdan.stanese@digilent.ro>
2025-05-26 12:55:23 +0300

    Added ddr-streaming workspace, for now.

---
+++ ws-Eclypse-Z7-ddr-streaming/calibration_reader/src/scope_calibration.h
@@ -0,0 +1,12 @@
+#ifndef SCOPE_CALIBRATION_H_   /* prevent circular inclusions */
+#define SCOPE_CALIBRATION_H_
+
+#include "zmod_scope_axi_configuration.h"
+#include "xstatus.h"
+
+#define ZMODSCOPE_ZMOD_PORT_A_VIO_GROUP 0
+#define ZMODSCOPE_ZMOD_PORT_B_VIO_GROUP 1
+
+XStatus ZmodScope_ReadCoefficientsFromDna(u32 ZmodPortVioGroup, ZmodScope_CalibrationCoefficients *FactoryCoefficients, ZmodScope_CalibrationCoefficients *UserCoefficients);
+
+#endif /* end of protection macro */
