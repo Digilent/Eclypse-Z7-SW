888b8395ee6151ad52187c1827c3a5b9238f1922
bog739 <bogdan.stanese@digilent.ro>
2025-07-10 16:12:12 +0300

    Updated .xsa file for v2025.1 Compile error: From dpmutil - i2c resources from macros are not expanded, even though PLATFORM_ZYNQ is defined, use direct names from the driver. Vitis v2025.1: Components manually created cannot be saved by checkin.py.

--- src/calibration_reader/src/UserConfig.cmake
+++ src/calibration_reader/src/UserConfig.cmake
@@ -1,31 +1,50 @@
-# Copyright (C) 2023 Advanced Micro Devices, Inc.  All rights reserved.
+# Copyright (C) 2023-2025 Advanced Micro Devices, Inc.  All rights reserved.
 # SPDX-License-Identifier: MIT
 cmake_minimum_required(VERSION 3.16)
+enable_language(C ASM CXX)
 
 ###    USER SETTINGS  START    ###
 # Below settings can be customized
-# User need to edit it manually as per their needs.
+# User needs to edit it manually as per their needs.
 ###    DO NOT ADD OR REMOVE VARIABLES FROM THIS SECTION    ###
 # -----------------------------------------
 # Add any compiler definitions, they will be added as extra definitions
-# Example adding VERBOSE=1 will pass -DVERBOSE=1 to the compiler.
+# Example : Adding VERBOSE=1 will pass -DVERBOSE=1 to the compiler.
 set(USER_COMPILE_DEFINITIONS
 ""
 )
 
 # Undefine any previously specified compiler definitions, either built in or provided with a -D option
-# Example adding MY_SYMBOL will pass -UMY_SYMBOL to the compiler.
+# Example : Adding MY_SYMBOL will pass -UMY_SYMBOL to the compiler.
 set(USER_UNDEFINED_SYMBOLS
 "__clang__"
 )
 
 
 # Add any directories below, they will be added as extra include directories.
-# Example 1: Adding /proj/data/include will pass -I/proj/data/include
+# Example 1: Adding /proj/data/include will pass -I/proj/data/include.
 # Example 2: Adding ../../common/include will consider the path as relative to this component directory.
 # Example 3: Adding ${CMAKE_SOURCE_DIR}/data/include to add data/include from this project.
 
 set(USER_INCLUDE_DIRECTORIES
+)
+set(USER_COMPILE_SOURCES
+"../main.c"
+"../manual_trigger.c"
+"../platform.c"
+"../s2mm_transfer.c"
+"../scope_calibration.c"
+"../test_stream_source.c"
+"../trigger.c"
+"../zmod_scope_axi_configuration.c"
+"../dpmutil/dpmutil.c"
+"../dpmutil/I2CHAL.c"
+"../dpmutil/PlatformMCU.c"
+"../dpmutil/syzygy.c"
+"../dpmutil/Zmod.c"
+"../dpmutil/ZmodADC.c"
+"../dpmutil/ZmodDAC.c"
+"../dpmutil/ZmodDigitizer.c"
 )
 
 # -----------------------------------------
@@ -39,7 +58,7 @@ set(USER_COMPILE_WARNINGS_EXTRA -Wextra)
 # Make all warnings into hard errors (-Werror)
 set(USER_COMPILE_WARNINGS_AS_ERRORS )
 
-# Check the code for syntax errors, but don't do anything beyond that. (-fsyntax-only)
+# Check the code for syntax errors, but don't do anything beyond that (-fsyntax-only)
 set(USER_COMPILE_WARNINGS_CHECK_SYNTAX_ONLY )
 
 # Issue all the mandatory diagnostics listed in the C standard (-pedantic)
@@ -53,7 +72,7 @@ set(USER_COMPILE_WARNINGS_INHIBIT_ALL )
 
 # -----------------------------------------
 
-# Optimization level   "-O0" [None] , "-O1" [Optimize] , "-O2" [Optimize More], "-O3" [Optimize Most] or "-Os" [Optimize Size]
+# Optimization level   "-O0" [None], "-O1" [Optimize] , "-O2" [Optimize More], "-O3" [Optimize Most] or "-Os" [Optimize Size]
 set(USER_COMPILE_OPTIMIZATION_LEVEL -O0)
 
 # Other flags related to optimization
@@ -61,15 +80,15 @@ set(USER_COMPILE_OPTIMIZATION_OTHER_FLAGS )
 
 # -----------------------------------------
 
-# Debug level "" [None], "-g1" [Minimum], "g2" [Default], "g3" [Maximim]
+# Debug level "" [None], "-g1" [Minimum], "g2" [Default], "g3" [Maximum]
 set(USER_COMPILE_DEBUG_LEVEL -g3)
 
-# Other flags releated to debugging
+# Other flags related to debugging
 set(USER_COMPILE_DEBUG_OTHER_FLAGS )
 
 # -----------------------------------------
 
-# Enable Profiling (-pg) (This feature is not supported currently)
+# Enable profiling (-pg) (This feature is not supported currently)
 # set(USER_COMPILE_PROFILING_ENABLE )
 
 # -----------------------------------------
@@ -79,7 +98,8 @@ set(USER_COMPILE_VERBOSE )
 
 # Support ANSI_PROGRAM (-ansi)
 set(USER_COMPILE_ANSI )
-
+set(USER_COMPILE_RELAXATION "-Wl,--no-relax")
+set(USER_COMPILE_GARBAGE "")
 # Add any compiler options that are not covered by the above variables, they will be added as extra compiler options
 # To enable profiling -pg [ for gprof ]  or -p [ for prof information ]
 set(USER_COMPILE_OTHER_FLAGS )
@@ -97,20 +117,20 @@ set(USER_LINK_NO_DEFAULT_LIBS )
 # Do not use the standard system startup files or libraries when linking. (-nostdlib)
 set(USER_LINK_NO_STDLIB )
 
-# Omit all symbol information (-s)
+# Omit all symbol information. (-s)
 set(USER_LINK_OMIT_ALL_SYMBOL_INFO )
 
 
 # -----------------------------------------
 
 # Add any libraries to be linked below, they will be added as extra libraries.
-# User need to update USER_LINK_DIRECTORIES below with these library paths.
+# User needs to update USER_LINK_DIRECTORIES below with these library search paths.
 set(USER_LINK_LIBRARIES
 )
 
 # Add any directories to look for the libraries to be linked.
 # Example 1: Adding /proj/compression/lib will pass -L/proj/compression/lib to the linker.
-# Example adding Adding ../../common/lib will consider the path as relative to this directory. and will pass the path to -L option.
+# Example 2: Adding ../../common/lib will consider the path as relative to this directory and will pass the path to -L option.
 set(USER_LINK_DIRECTORIES
 )
 
@@ -119,7 +139,7 @@ set(USER_LINK_DIRECTORIES
 set(USER_LINKER_SCRIPT "${CMAKE_SOURCE_DIR}/lscript.ld")
 
 # Add linker options to be passed, they will be added as extra linker options
-# Example : adding -s will pass -s to the linker.
+# Example : Adding -s will pass -s to the linker.
 set(USER_LINK_OTHER_FLAGS
 )
 
@@ -155,3 +175,29 @@ set(USER_LINK_OPTIONS
     " ${USER_LINKER_OMIT_ALL_SYMBOL_INFO}"
     " ${USER_LINK_OTHER_FLAGS}"
 )
+if(("${CMAKE_SYSTEM_PROCESSOR}" STREQUAL "microblaze") OR ("${CMAKE_SYSTEM_PROCESSOR}" STREQUAL "microblaze_riscv"))
+	if(USER_COMPILE_RELAXATION)
+		string(FIND "${CMAKE_C_LINK_FLAGS}" "-Wl,--no-relax" POSITION)
+		if(POSITION EQUAL -1)
+		    set(CMAKE_C_LINK_FLAGS "  -Wl,--no-relax ${CMAKE_C_LINK_FLAGS}" CACHE STRING "CMAKE C LINK FLAGS" FORCE)
+		    set(CMAKE_ASM_LINK_FLAGS "  -Wl,--no-relax ${CMAKE_ASM_LINK_FLAGS}" CACHE STRING "CMAKE ASM LINK FLAGS" FORCE)
+		    set(CMAKE_CXX_LINK_FLAGS "  -Wl,--no-relax ${CMAKE_CXX_LINK_FLAGS}" CACHE STRING "CMAKE CXX LINK FLAGS" FORCE)
+		endif()
+	else()
+		string(REPLACE "-Wl,--no-relax" "" CMAKE_C_LINK_FLAGS "${CMAKE_C_LINK_FLAGS}")
+		string(REPLACE "-Wl,--no-relax" "" CMAKE_ASM_LINK_FLAGS "${CMAKE_ASM_LINK_FLAGS}")
+		string(REPLACE "-Wl,--no-relax" "" CMAKE_CXX_LINK_FLAGS "${CMAKE_CXX_LINK_FLAGS}")
+	endif()
+	if(USER_COMPILE_GARBAGE)
+		string(FIND "${CMAKE_C_FLAGS}" "-ffunction-sections -fdata-sections" POSITION)
+		if(POSITION EQUAL -1)
+		    set(CMAKE_C_FLAGS " -ffunction-sections -fdata-sections ${CMAKE_C_FLAGS}" CACHE STRING "CMAKE C FLAGS" FORCE)
+		    set(CMAKE_CXX_FLAGS " -ffunction-sections -fdata-sections ${CMAKE_CXX_FLAGS}" CACHE STRING "CMAKE CXX FLAGS" FORCE)
+		    set(CMAKE_ASM_FLAGS " -ffunction-sections -fdata-sections ${CMAKE_ASM_FLAGS}" CACHE STRING "CMAKE ASM FLAGS" FORCE)
+		endif()
+	else()
+		string(REPLACE "-ffunction-sections -fdata-sections" "" CMAKE_C_FLAGS "${CMAKE_C_FLAGS}")
+		string(REPLACE "-ffunction-sections -fdata-sections" "" CMAKE_CXX_FLAGS "${CMAKE_ASM_FLAGS}")
+		string(REPLACE "-ffunction-sections -fdata-sections" "" CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS}")
+	endif()
+endif()
