ed59a0419b302cab5d9e00f90c318362a4d1d3bb
bog739 <bogdan.stanese@digilent.ro>
2025-08-18 23:04:02 +0300

    Added in .gitignore lines to exclude dfx_runtime.txt and .Xil from sw/.
    Manually created components crash checkin.py when copying
    their files, making a mess in sw/src/.

--- src/calibration_reader/src/manual_trigger.h
+++
@@ -1,8 +0,0 @@
-#ifndef MANUAL_TRIGGER_LOCAL_H_   /* prevent circular inclusions */
-#define MANUAL_TRIGGER_LOCAL_H_
-
-#include "ManualTrigger.h"
-
-void ManualTriggerIssueTrigger(ManualTrigger *InstPtr);
-
-#endif /* end of protection macro */
